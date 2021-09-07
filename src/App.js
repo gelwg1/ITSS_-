66d459a0a3b77473d4e63462f5918fcc8e8c4053
gelwg1 <gelwg1@gmail.com>
2021-09-08 00:31:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">DashBoard</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <DashBoard />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
-}
-
+};
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+const DashBoard = () => {
+  return (
+    <div>
+      <h2>DashBoard</h2>
+    </div>
+  );
+};
 export default App;
